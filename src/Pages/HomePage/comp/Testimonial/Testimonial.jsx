66b10e29d7a364c498b4f1c4ad52c739b25ac0f4
Md Parvej <mdparvejme24@gmail.com>
2024-10-05 00:47:59 +0600

    update review section

--- src/Pages/HomePage/comp/Testimonial/Testimonial.jsx
+++ src/Pages/HomePage/comp/Testimonial/Testimonial.jsx
@@ -1,50 +1,70 @@
 import React, { useEffect, useState } from "react";
+import SectionHeader from "../../../../Components/Shared/SectionHeader/SectionHeader";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/navigation";
-
-import { Navigation } from "swiper/modules";
+import "swiper/css/pagination";
 
 import { Rating } from "@smastrom/react-rating";
 import "@smastrom/react-rating/style.css";
 
 export default function Testimonial() {
   const [reviews, setReviews] = useState([]);
+
   useEffect(() => {
     fetch("reviews.json")
       .then((res) => res.json())
       .then((data) => setReviews(data));
   }, []);
+
   return (
-    <div className="bg-[#181818]">
-      <div className="container mx-auto py-32">
-        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+    <div className="py-20">
+      <SectionHeader title={"Testimonial"} subTitle={"Client Feedback"} />
+
+      <div className="container mx-auto pt-14">
+        <Swiper
+          slidesPerView={2}
+          spaceBetween={20}
+          className="mySwiper"
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+        >
           {reviews?.map((review) => (
             <SwiperSlide key={review._id}>
-              <div className="p-10 bg-white rounded-2xl">
-                <div className="flex items-center gap-5">
+              <div className="p-5 rounded-xl shadow-md border min-h-[250px]">
+                {/* Upper part: Profile and Info */}
+                <div className="flex gap-5">
                   <img
-                    src="https://gramentheme.com/html/fresheat/assets/img/testimonial/testimonialProfile1_1.png"
-                    alt=""
-                    className="rounded-full"
+                    src={
+                      "https://gramentheme.com/html/fresheat/assets/img/testimonial/testimonialProfile1_1.png"
+                    }
+                    alt={review.name}
+                    className="w-[70px] h-[70px] rounded-full"
                   />
                   <div>
-                    <h3 className="text-2xl font-bold capitalize">
-                      {review.name}
-                    </h3>
-                    <p>Full Stack Developer</p>
-                    <p className="">
-                      <Rating
-                        style={{ maxWidth: 100 }}
-                        value={review.rating}
-                        readOnly
-                      />
-                    </p>
+                    <h3 className="text-2xl font-bold">{review.name}</h3>
+                    <p className="text-gray-600">{"Web Designer"}</p>
+                    <Rating
+                      style={{ maxWidth: 100 }}
+                      value={review.rating}
+                      readOnly
+                    />
                   </div>
                 </div>
-                <p className="mt-5 text-gray-500">{review.details}</p>
+
+                {/* Review Details */}
+                <p className="mt-5">{review.details}</p>
               </div>
             </SwiperSlide>
           ))}
