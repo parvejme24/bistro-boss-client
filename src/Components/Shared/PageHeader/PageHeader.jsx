eef9bad8d4725a970bd30fc7985639de4aa3fe6b
Md Parvej <mdparvejme24@gmail.com>
2024-10-07 18:21:40 +0600

    create page header and added fav icon

---
+++ src/Components/Shared/PageHeader/PageHeader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PageHeader({ title }) {
+  return (
+    <div className="bg-[url('https://gramentheme.com/html/fresheat/assets/img/bg/breadcumb.jpg')] bg-no-repeat max-h-[250px] h-[250px] flex justify-center items-center">
+      <h2 className="text-white text-3xl md:text-5xl font-bold uppercase">{title}</h2>
+    </div>
+  );
+}
---
+++ src/Pages/MenuPage/MenuPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PageHeader from "../../Components/Shared/PageHeader/PageHeader";
+
+export default function MenuPage() {
+  return (
+    <div>
+      <PageHeader title={"Menu Page"} />
+      <div className="cntainer mx-auto"></div>
+    </div>
+  );
+}
