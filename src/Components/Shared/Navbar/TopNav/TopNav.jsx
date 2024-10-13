712199fb2ad7b4d6f24b0e580a5bcdf34b4f9926
Md Parvej <mdparvejme24@gmail.com>
2024-10-13 21:07:24 +0600

    added time in top nav

--- src/Components/Shared/Navbar/TopNav/TopNav.jsx
+++ src/Components/Shared/Navbar/TopNav/TopNav.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { FaFacebook } from "react-icons/fa";
+import { FaClock, FaFacebook } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function TopNav() {
   return (
     <div className="bg-[#EB0029] py-2">
-      <div className="container mx-auto">
-        <div className="flex justify-end items-center gap-5">
+      <div className="container mx-auto flex items-center justify-between flex-wrap">
+        <div>
+          <p className="text-[#F7BFD4] flex items-center gap-2">
+            <FaClock /> 09:00 am - 06:00 pm
+          </p>
+        </div>
+        <div className="flex items-center gap-5">
           <p className="text-[#F7BFD4]">Follow Us:</p>
           <ul className="text-[#F7BFD4] flex items-center gap-3">
             <li>
